cec3b0320994b5c922aed21b49d1787075332094
CamDiggidyDawg <camrcrichton@gmail.com>
2023-03-03 15:42:10 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread  # from code institue love-sandwiches walkthrough
+# import Credentials class code from code institute love-sandwiches
+from google.oauth2.service_account import Credentials
+
+# SCOPE name and code for contastant from the code institute
+# love-sandwiches walkthrough
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# CREDS name and code for constant from the code institute
+# love-sandwiches walkthough
+CREDS = Credentials.from_service_account_file('creds.json')
+# SCOPED_CREDS name and code for constant from the code institute
+# love-sandwiches walkthrough
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+# GSPREAD_CLIENT name and code for constant from the code institute
+# love-sandwiches walkthrough
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+# SHEET name and code for constant from the code institute
+# love-sandwiches walkthrough
+SHEET = GSPREAD_CLIENT.open('Transporter')
+
+# transport_details & data variable ideas and code from the code institute
+# love-sandwiches walkthrough
+transport_details = SHEET.worksheet('transport_details')
+
+data = transport_details.get_all_values()
+
+print(data)
